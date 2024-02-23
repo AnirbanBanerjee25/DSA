4bd57bef3430ea442d3b5b2e9c67d9b8a7544998
Anirban Banerjee <anirban.banerjee2016@gmail.com>
2024-02-23 10:33:37 +0530

    Add files via upload

---
+++ Stack_Collections_Framework.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Stack_Collections_Framework {
+    public static void main(String args[])
+    {
+        Stack<Integer>  s=new Stack<>();
+
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        s.push(40);
+        s.push(50);
+
+        while(!s.isEmpty())
+        {
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
